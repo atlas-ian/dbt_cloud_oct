f73d931d392a1827258bb42e9f4be804621291e4
Adarsh N <adarshn54688@gmail.com>
2025-11-28 07:24:06 +0000

    stg_nations error cleared

--- models/stage/stg_nations.sql
+++ models/stage/stg_nations.sql
@@ -10,9 +10,8 @@ with nation as (
         n_nationkey as nation_id,
         n_name as name,
         n_regionkey as region_id,
-        updated_at,
-        {{dbt_meta()}}
-    from {{ source ('src', 'nations') }}
+        updated_at
+        from {{ source ('src', 'nations') }}
 )
 
 select *
